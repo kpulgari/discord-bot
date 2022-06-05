16e4330f3b9cfd03070a7dd9526b35ed27cd9f67
Kaushik Pulgari <kpulg2@illinois.edu>
2022-06-04 21:26:09 -0500

    inital commit

---
+++ main.py
@@ -0,0 +1,87 @@
+from discord.ext import commands
+from dotenv import load_dotenv
+import os
+import random
+# import asyncio
+
+# 6/04 - QUIZ: recieve user input
+
+load_dotenv()
+
+bot = commands.Bot(command_prefix='!')
+bot.videos = ['https://www.youtube.com/watch?v=57ZODYKHhUg&list=RD57ZODYKHhUg&start_radio=1', 'https://www.youtube.com/watch?v=46teMg199cw&list=RD57ZODYKHhUg&index=6',
+'https://www.youtube.com/watch?v=kuYVwlkuBoQ&list=RDkuYVwlkuBoQ&start_radio=1']
+bot.happylist = []
+
+allowed_commands = ['hello', 'goodbye', 'music', 'happy', 'sad', 'calc', 'rand', 'quiz']
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send("Hello, " + ctx.author.display_name + "!")
+    
+@bot.command()
+async def goodbye(ctx):
+    await ctx.send("Goodbye, " + ctx.author.display_name + "!")
+
+@bot.command()
+async def music(ctx):
+    await ctx.send(random.choice(bot.videos))
+
+@bot.command()
+async def happy(ctx, *, item):
+    await ctx.send("Awesome!")
+    bot.happylist.append(item)
+    print(bot.happylist)
+
+@bot.command()
+async def sad(ctx):
+    await ctx.send("Hope this makes you feel better!")
+    await ctx.send(random.choice(bot.happylist))
+
+@bot.command()
+async def calc(ctx, x: float, fn, y: float):
+    if fn == '+':
+        await ctx.send(x + y)
+    elif fn == '-':
+        await ctx.send(x - y)
+    elif fn == '*':
+        await ctx.send(x * y)
+    elif fn == '/':
+        await ctx.send(x / y)
+    elif fn == '%':
+        await ctx.send(x % y)
+    else:
+        await ctx.send("bad operand")
+
+@bot.command()
+async def rand(ctx, num_one: int, num_two: int):
+    if num_one > num_two:
+        await ctx.send("bad operand")
+    else:
+        await ctx.send(random.randint(num_one, num_two))
+
+@bot.command()
+async def quiz(ctx):
+    one = random.randint(0, 5)
+    two = random.randint(0, 5)
+    ans = one + two
+    await ctx.send(f"What is {one} + {two}?")
+
+    def check(msg):
+        return msg == ans
+        
+    msg = await bot.wait_for('message', check=check)
+    if msg:
+        await ctx.send("GOOD")
+    else:
+        await ctx.send("BAD")
+
+@bot.command()
+async def cmds(ctx):
+    list = ''
+    for num, cmd in enumerate(allowed_commands):
+        list += str(num + 1) + ". " + cmd.title() + "\n"
+    await ctx.send(list)
+
+password = os.getenv('password')
+bot.run(password)
